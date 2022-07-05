77fec235871a66b3749559ca03d23f60c98720bf
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-05 13:04:40 +0100

    get random anime quote

--- js/main.js
+++ js/main.js
@@ -3,6 +3,25 @@ const character = document.getElementById('character');
 const anime_name = document.getElementById('anime_name');
 const random_btn = document.getElementById('random_btn');
 const search_btn = document.getElementById('search_btn');
-const url = 'https://animechan.vercel.app/api/random';
+
+//API URLs
+const random_anime = 'https://animechan.vercel.app/api/random';
+const anime_by_title = 'https://animechan.vercel.app/api/quotes/anime?title=';
+const anime_by_character = 'https://animechan.vercel.app/api/quotes/character?name=';
+
+random_btn.addEventListener('click', () => {
+    getRandomQuote(random_anime);
+});
 
 
+//Get random anime quote
+function getRandomQuote(url){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        quote.innerHTML = data.quote;
+        character.innerHTML = data.character;
+        anime_name.innerHTML = data.anime;
+    })
+    .catch(error => console.log(error));
+}
