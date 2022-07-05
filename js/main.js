a47e9378f916b01fbc1f8a218a744342974507d2
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-05 12:52:47 +0100

    add javascript

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const quote = document.getElementById('quote');
+const character = document.getElementById('character');
+const anime_name = document.getElementById('anime_name');
+const random_btn = document.getElementById('random_btn');
+const search_btn = document.getElementById('search_btn');
+const url = 'https://animechan.vercel.app/api/random';
+
+
