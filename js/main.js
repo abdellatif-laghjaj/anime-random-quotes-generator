772208709596e4980af045fadd4c0f5202d85e66
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-05 13:32:09 +0100

    search anime by name or character

--- js/main.js
+++ js/main.js
@@ -4,16 +4,28 @@ const anime_name = document.getElementById('anime_name');
 const random_btn = document.getElementById('random_btn');
 const input = document.querySelector('input');
 const search_btn = document.getElementById('search_btn');
+const search_by = document.getElementById('search_by');
 
 //API URLs
 const random_anime = 'https://animechan.vercel.app/api/random';
 const anime_by_title = 'https://animechan.vercel.app/api/quotes/anime?title=';
 const anime_by_character = 'https://animechan.vercel.app/api/quotes/character?name=';
 
+//values
+let search_value = '';
+let search_by_value = '';
+
 random_btn.addEventListener('click', () => {
     getRandomQuote(random_anime);
 });
 
+
+search_btn.addEventListener('click', () => {
+    search_value = input.value;
+    search_by_value = search_by.value;
+    if (search_by_value === 'by_name') getAnimeByTitle(anime_by_title + search_value);
+    getQuoteByCharacter(anime_by_character + search_value);
+});
 
 //Get random anime quote
 function getRandomQuote(url){
@@ -25,4 +37,27 @@ function getRandomQuote(url){
         anime_name.innerHTML = data.anime;
     })
     .catch(error => console.log(error));
+}
+
+//Get anime quote by title
+function getQuoteByTitle(url){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        quote.innerHTML = data.quote;
+        character.innerHTML = data.character;
+        anime_name.innerHTML = data.anime;
+    })
+    .catch(error => console.log(error));
+}
+
+//Get anime quote by character
+function getQuoteByCharacter(url){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        quote.innerHTML = data.quote;
+        character.innerHTML = data.character;
+        anime_name.innerHTML = data.anime;
+    }).catch(error => console.log(error));
 }
